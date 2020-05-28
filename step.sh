539181b91fd1068d33fdab6608deb8fee975045d
Nicolas Bellon <nicolas@foodvisor.io>
2020-05-28 18:33:32 +0200

    Inital commit

---
+++ step.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+git clone git@github.com:Foodvisor/bundle_updater.git
+xcodebuild -project bundle_updater/bundle_updater.xcodeproj -configuration Release
+bundle_updater/build/Release/bundle_updater -env "${env}" -version "${version}" -token "${token}"
