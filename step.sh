3579c82223becb33a1b0d3a078bd823013c8ae9e
BDurand <benoit.durand3@gmail.com>
2024-08-26 11:57:48 +0200

    Update step.sh

--- step.sh
+++ step.sh
@@ -2,4 +2,5 @@
 set -ex
 git clone git@github.com:Foodvisor/bundle_updater.git
 xcodebuild -project bundle_updater/bundle_updater.xcodeproj -configuration Release
+pwd
 bundle_updater/build/Release/bundle_updater -env "${env}" -version "${version}" -mail "${mail}" -password "${password}" -lang "${lang}"
